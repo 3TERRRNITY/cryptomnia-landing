b561b36e14c55a20f6d5a6665bafdb91b6daf284
3TERRRNITY <qertisww@gmail.com>
2023-10-07 16:44:22 +0300

    our vision layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Hero from "./components/Hero/Hero";
 import Layout from "./components/Layout/Layout";
 import Navbar from "./components/Navbar/Navbar";
+import OurVision from "./components/OurVision/OurVision";
 
 function App() {
   return (
     <Layout>
       <Navbar />
       <Hero />
+      <OurVision />
     </Layout>
   );
 }
