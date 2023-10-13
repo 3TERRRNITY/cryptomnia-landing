ceec5df067a02824a358da3b04e743877cdd344e
3TERRRNITY <qertisww@gmail.com>
2023-10-13 12:31:17 +0300

    adaptive navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,57 @@
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.scss";
+import NavbarMobile from "../NavbarMobile/NavbarMobile";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+  const handleCloseMobileNav = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
   return (
-    <div className={styles.navbar}>
-      <div className={styles.navbar__logo} />
-      <ul className={styles.navbar__items}>
-        <li className={styles.navbar__item}>
-          <a href="#vision">Our vision</a>
-        </li>
-        <li className={styles.navbar__item}>
-          <a href="#experts">Our experts</a>
-        </li>
-        <li className={styles.navbar__item}>
-          <a href="#services">Our services</a>
-        </li>
-        <li className={styles.navbar__item}>
-          <a href="cases">Our cases</a>
-        </li>
-        <li className={styles.navbar__item}>
-          <a href="#ABS">Api/bridges/SDK</a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.navbar__logo} />
+        <ul className={styles.navbar__items}>
+          <li className={styles.navbar__item}>
+            <a href="#vision">Our vision</a>
+          </li>
+          <li className={styles.navbar__item}>
+            <a href="#experts">Our experts</a>
+          </li>
+          <li className={styles.navbar__item}>
+            <a href="#services">Our services</a>
+          </li>
+          <li className={styles.navbar__item}>
+            <a href="cases">Our cases</a>
+          </li>
+          <li className={styles.navbar__item}>
+            <a href="#ABS">Api/bridges/SDK</a>
+          </li>
+        </ul>
+        <div className={styles.navbar__mobile} onClick={handleOpen}>
+          <img
+            className={styles.navbar__mobile__icon}
+            src="/assets/icons/menu.svg"
+          />
+        </div>
+      </div>
+      {isOpen && <NavbarMobile closeMobileNav={handleCloseMobileNav} />}
+    </>
   );
 };
 
---
+++ src/components/NavbarMobile/NavbarMobile.jsx
@@ -0,0 +1,43 @@
+import styles from "./NavbarMobile.module.scss";
+
+const NavbarMobile = ({ closeMobileNav }) => {
+  const handleLinkClick = () => {
+    closeMobileNav();
+  };
+  return (
+    <div className={styles.navbarMobile}>
+      <a href="/" onClick={handleLinkClick}>
+        <div className={styles.navbarMobile__logo} />
+      </a>
+      <ul className={styles.navbarMobile__items}>
+        <li className={styles.navbar__item}>
+          <a href="#vision" onClick={handleLinkClick}>
+            Our vision
+          </a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#experts" onClick={handleLinkClick}>
+            Our experts
+          </a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#services" onClick={handleLinkClick}>
+            Our services
+          </a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#cases" onClick={handleLinkClick}>
+            Our cases
+          </a>
+        </li>
+        <li className={styles.navbar__item}>
+          <a href="#ABS" onClick={handleLinkClick}>
+            Api/bridges/SDK
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavbarMobile;
