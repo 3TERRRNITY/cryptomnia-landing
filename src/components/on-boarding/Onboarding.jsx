7c46afc9bece33931dc51f87f80a8fc4dcbeada5
3TERRRNITY <qertisww@gmail.com>
2023-10-14 12:35:40 +0300

    ui-fixes

--- src/components/on-boarding/Onboarding.jsx
+++ src/components/on-boarding/Onboarding.jsx
@@ -3,31 +3,34 @@ import { ONBOARDING_CARDS } from "../../constants/data";
 
 const Onboarding = () => {
   return (
-    <div className={styles.onboarding} id="cases">
-      <div className={styles.onboarding__title}>on-boarding process</div>
-      <div className={styles.onboarding__cards}>
-        {ONBOARDING_CARDS.map(({ index, description, image }) => (
-          <div
-            className={`${styles.onboarding__card} ${
-              index === 5 ? styles["onboarding__card__big"] : ""
-            }`}
-            key={index}
-          >
-            <div className={styles.onboarding__card__title}>0{index}</div>
-            <div className={styles.onboarding__card__container}>
-              <img
-                src={image}
-                alt="icon"
-                className={styles.onboarding__card__icon}
-              />
-              <div className={styles.onboarding__card__description}>
-                {description}
+    <>
+      <div className={styles.background} />
+      <div className={styles.onboarding} id="cases">
+        <div className={styles.onboarding__title}>on-boarding process</div>
+        <div className={styles.onboarding__cards}>
+          {ONBOARDING_CARDS.map(({ index, description, image }) => (
+            <div
+              className={`${styles.onboarding__card} ${
+                index === 5 ? styles["onboarding__card__big"] : ""
+              }`}
+              key={index}
+            >
+              <div className={styles.onboarding__card__title}>0{index}</div>
+              <div className={styles.onboarding__card__container}>
+                <img
+                  src={image}
+                  alt="icon"
+                  className={styles.onboarding__card__icon}
+                />
+                <div className={styles.onboarding__card__description}>
+                  {description}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
